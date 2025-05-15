06f6b7395e6c6c49bd88c0b241649e34e8f79bd5
Ilyas Khallouki <khalloukielias@gmail.com>
2025-05-15 12:51:21 +0100

    Fixed bug

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Header from "./Header";
+// import Sidebar from "./Sidebar";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <Box
@@ -12,7 +13,8 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
       overflow: "hidden",
     }}
   >
-    <Box sx={{ flex: 1, display: "flex"}}>
+    {/* <Sidebar /> */}
+    <Box sx={{ flex: 1, display: "flex", flexDirection: "column" }}>
       <Header />
       <Box sx={{ flex: 1, background: "#fff" }}>
         {children}
